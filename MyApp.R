78fe8261420169015fd29591290dd58309677ff2
AMalta <alex.oceano@gmail.com>
2025-05-06 13:14:24 -0300

    Add files via upload

---
+++ MyApp.R
@@ -0,0 +1,37 @@
+library(bslib)
+library(shiny)
+
+
+ui <- page_sidebar(
+  "Meu app",
+  
+  sidebar = sidebar(
+  numericInput(
+    "v1",
+    "Digite o primeiro valor: ",value = ""
+  ),
+  numericInput(
+    "v2",
+    "Digite o segundo valor: ",value = ""
+  ),
+  
+  numericInput(
+    "v3",
+    "Digite o terceiro valor: ",value = ""
+  )
+),
+
+plotOutput("pizza")
+
+)
+
+server <- function(input, output, session) {
+  output$pizza = renderPlot({
+    x = c(input$v1,input$v2,input$v3)
+    pie(x)
+    
+  })
+  
+}
+
+shinyApp(ui, server)
